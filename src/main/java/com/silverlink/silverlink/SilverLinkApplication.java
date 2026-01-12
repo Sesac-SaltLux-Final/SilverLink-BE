d843d516d98bfaa63e993ea1b7cb309b582b4caf
hyunn9799 <underwater0518@gmail.com>
2026-01-12 11:10:24 +0900

    first commit

---
+++ src/main/java/com/silverlink/silverlink/SilverLinkApplication.java
@@ -0,0 +1,13 @@
+package com.silverlink.silverlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilverLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silverlink/silverlink/SilverLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silverlink.silverlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
