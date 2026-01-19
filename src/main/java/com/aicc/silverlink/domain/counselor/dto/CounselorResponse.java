2486162a30c9606f4f5b83e506505a7b2f846580
gosiwang <harmoey@naver.com>
2026-01-19 17:44:07 +0900

    feat:상담사 기능구현#9

--- src/main/java/com/aicc/silverlink/domain/counselor/dto/CounselorResponse.java
+++ src/main/java/com/aicc/silverlink/domain/counselor/dto/CounselorResponse.java
@@ -1,4 +1,44 @@
 package com.aicc.silverlink.domain.counselor.dto;
 
+import com.aicc.silverlink.domain.counselor.entity.Counselor;
+import com.aicc.silverlink.domain.user.entity.User;
+import com.aicc.silverlink.domain.user.entity.UserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@NoArgsConstructor
 public class CounselorResponse {
+
+    private Long id;
+
+    private String name;
+    private String loginId;
+    private String email;
+    private String phone;
+
+    private String employeeNo;
+    private String department;
+
+    private UserStatus status;
+    private String officePhone;
+
+    private String admDongCode;
+
+    public static CounselorResponse from(Counselor counselor){
+        User user = counselor.getUser();
+
+        return CounselorResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .loginId(user.getLoginId())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .employeeNo(counselor.getEmployeeNo())
+                .build();
+    }
 }
