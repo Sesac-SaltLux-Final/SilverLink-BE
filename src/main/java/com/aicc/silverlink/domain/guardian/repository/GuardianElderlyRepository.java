b4bf5555c7ad5ee43580bdec1df59d810761f974
gosiwang <harmoey@naver.com>
2026-01-20 09:16:27 +0900

    feat:상담사 기능구현#9

---
+++ src/main/java/com/aicc/silverlink/domain/guardian/repository/GuardianElderlyRepository.java
@@ -0,0 +1,8 @@
+package com.aicc.silverlink.domain.guardian.repository;
+
+import com.aicc.silverlink.domain.guardian.entity.GuardianElderly;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuardianElderlyRepository extends JpaRepository<GuardianElderly, Long>
+{
+}
--- src/main/java/com/aicc/silverlink/domain/guardian/repository/GuardianRepository.java
+++ src/main/java/com/aicc/silverlink/domain/guardian/repository/GuardianRepository.java
@@ -1,7 +1,10 @@
 package com.aicc.silverlink.domain.guardian.repository;
 
+import com.aicc.silverlink.domain.guardian.entity.Guardian;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class GuardianRepository {
+public interface GuardianRepository extends JpaRepository<Guardian,Long> {
+
 }
