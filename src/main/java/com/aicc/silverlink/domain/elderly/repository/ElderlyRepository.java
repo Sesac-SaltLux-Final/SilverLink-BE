c2bafc4be69494fb7e11b559134dc1cb80a74a06
gosiwang <harmoey@naver.com>
2026-01-20 14:25:03 +0900

    feat:보호자 기능 구현중#30

--- src/main/java/com/aicc/silverlink/domain/elderly/repository/ElderlyRepository.java
+++ src/main/java/com/aicc/silverlink/domain/elderly/repository/ElderlyRepository.java
@@ -1,7 +1,9 @@
 package com.aicc.silverlink.domain.elderly.repository;
 
+import com.aicc.silverlink.domain.elderly.entity.Elderly;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ElderlyRepository {
+public interface ElderlyRepository extends JpaRepository<Elderly,Long> {
 }
