aec6427ddea4a9565b2f3e4dc9b60ead18a82e5c
J <dlatl97531@gmail.com>
2026-01-21 17:39:53 +0900

    cons35-ent 기능 구현

--- src/main/java/com/aicc/silverlink/domain/consent/controller/ConsentController.java
+++
@@ -1,9 +0,0 @@
-package com.aicc.silverlink.domain.consent.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RestController
-@RequestMapping("/api/consents")
-public class ConsentController {
-}
